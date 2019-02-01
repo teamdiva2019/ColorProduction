04b0b36ffc120310dc95cc0aa2b4872eeb857b92
MughilM <mughilan.muthupari@gmail.com>
2019-02-01 14:37:50 -0500

    Fixed the interpolation scheme to properly do a 2D interpolation given corners of a box. Vectors should be placed correctly now.

--- MainFiles/WriteToFGA.py
+++ MainFiles/WriteToFGA.py
@@ -1,5 +1,89 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import product
+from mpl_toolkits.mplot3d import Axes3D
+import sys
+import time
+
+def cart2sph(x, y, z):
+    hxy = np.hypot(x, y)
+    r = np.hypot(hxy, z)
+    el = np.arctan2(z, hxy)
+    az = np.arctan2(y, x)
+    if az < 0:
+        az = 2 * np.pi + az
+    return r, el, az
+
+# Function to find the 4 points that make the
+# box that a target lat and lon falls in.
+# Returns the weights as well.
+# Returns a list of tuples in
+# (topLeft, topRight, bottomLeft, bottomRight) order.
+def findBoxPointsAndWeights(
+        targetLat: float,
+        targetLon: float,
+        latitudes: np.array,
+        longitudes: np.array):
+    upperLat = latitudes.searchsorted(targetLat)
+    rightLon = longitudes.searchsorted(targetLon) % len(longitudes)
+    leftLon = rightLon - 1
+
+    # Longitude weights are normal
+    # TODO: Handle the case where longitudal points don't start at 0.
+    if leftLon == -1:
+        diff = 2 * np.pi - longitudes[leftLon]
+    else:
+        diff = longitudes[rightLon] - longitudes[leftLon]
+    weightLon = (targetLon - longitudes[leftLon]) / diff
+
+    # If we're off the ends, then the four points
+    # will have the same latitude, but 4 different
+    # longitudes.
+    if upperLat == 0 or upperLat == len(latitudes):
+        # Get the opposite longitudes
+        oppRightLon = (rightLon + len(longitudes) // 2) % len(longitudes)
+        oppLeftLon = oppRightLon - 1
+        # In the 3D world, this would result in an
+        # hourglass shape, but this is intended as it does
+        # not hamper the interpolations...
+        # If upperlat is the length, then change it to the last
+        # element.
+        if upperLat == len(latitudes):
+            upperLat = -1
+        # The latitude weight works a tad
+        # differently, because we now technically
+        # go over the pole.
+
+        # North pole:
+        if upperLat == -1:
+            weightLat = (targetLat - latitudes[upperLat]) / (2 * (np.pi/2 - latitudes[upperLat]))
+        # South pole:
+        else:
+            # The subtraction turns into addition b/c negative.
+            # Likewise with the addition turning into sub.
+            # Net result is we have positive values.
+            # NOTE: Even though it looks like it, it's not the same
+            # as above.
+            weightLat = (latitudes[upperLat] - targetLat) / (2 * (np.pi/2 + latitudes[upperLat]))
+
+        return [
+            (upperLat, leftLon),
+            (upperLat, rightLon),
+            (upperLat, oppLeftLon),
+            (upperLat, oppRightLon)
+        ], weightLat, weightLon
+    # Otherwise, calculate the lower latitude
+    # as normal
+    lowerLat = upperLat - 1
+    # Latitude weight is normal
+    weightLat = (targetLat - latitudes[upperLat]) / (latitudes[upperLat] - latitudes[lowerLat])
+    return [
+        (upperLat, leftLon),
+        (upperLat, rightLon),
+        (lowerLat, leftLon),
+        (lowerLat, rightLon)
+    ], weightLat, weightLon
+
 
 # Function to find the closest 2 points to another point
 def find2Points(p, pointSpace, vectSpace):
@@ -12,9 +96,10 @@ def writeFGAFile(data, radius, resStep, padWidth=1):
     # the resolution step...user inputs.
     # Also calculate diameter in res units
     DRes = int(2 * radius / resStep + 1)
+    print('DRes:', DRes)
     # Set scaling values
-    windScale = 40
-    gravScale = 40
+    windScale = 30
+    gravScale = 30
 
     allVariables = data.variables
 
@@ -49,13 +134,19 @@ def writeFGAFile(data, radius, resStep, padWidth=1):
     # lonPoints = np.arange(0, 360 - 360 / numLonPoints, 360 / numLonPoints)
 
     u, v = allVariables[varNames[0]][0], allVariables[varNames[1]][0]
+    # print(u[:2], v[:2])
+    # plt.quiver(lonPoints * 180 / np.pi, latPoints * 180 / np.pi, u, v)
+    # plt.savefig('plotDownSample.png', dpi=300)
+    # plt.show()
+
+
 
     ################# TRANSFORM TO 3D VECTORS ##################
     print('Transforming to 3D vectors...')
     vects3D = np.zeros((numLatPoints, numLonPoints, 3))
     vects3D[:, :, 0] = u
     vects3D[:, :, 1] = v
-
+    # print('Initial 3D vectors:', vects3D)
     # Make matrix of exact locations of the vectors
     exactVectLocs = np.zeros((numLatPoints, numLonPoints, 3))
 
@@ -85,7 +176,9 @@ def writeFGAFile(data, radius, resStep, padWidth=1):
             # (x,y,0) ==> (-y,x,0)
             np.dot(rotMatrix, np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]), out=rotMatrix)
             # Apply the entire rotation matrix
+            # print(vects3D[i,j], '\t\t==> ', end='')
             vects3D[i, j] = np.dot(rotMatrix, vects3D[i, j])
+            # print(vects3D[i,j], '\t\t==> ', end='')
 
             ### Calculate the 2d point. Basically spherical coordinates
             exactVectLocs[i, j] = radius * np.array([
@@ -93,7 +186,12 @@ def writeFGAFile(data, radius, resStep, padWidth=1):
                 np.sin(np.pi / 2 - lat) * np.sin(lon),
                 np.cos(np.pi / 2 - lat)
             ])
+            # print('at', exactVectLocs[i, j], '({:.2f}, {:.2f})'.format(lat * 180 / np.pi, lon * 180 / np.pi))
     print('Transform complete!')
+    # print('3D Transformation after:', vects3D)
+    # print()
+    # print('Exact vector locations:', exactVectLocs)
+    # sys.exit(1)
 
     ################# GRAVITY ##################
 
@@ -111,79 +209,90 @@ def writeFGAFile(data, radius, resStep, padWidth=1):
     fgaVectors *= gravScale
     print('Application complete!')
 
-    ######################################################
+    ################# INTERPOLATION ######################
 
-    # It should be trivial that the closest 2 defined points
-    # to another point will be on the same xy-plane. Therefore,
-    # we can cycle through each latitude level and interpolate
-    # around the longitudes. The interpolation will be linear
-    # in the direction i.e. a weighted average.
-    # fgaVectors = np.zeros((DRes, DRes, DRes, 3), dtype=float)
     print('Interpolating vectors...')
-    for i, lat in enumerate(latPoints, 0):
-        exactLats = exactVectLocs[i]
-        vects3DLats = vects3D[i]
-        # Now, we need to find the points on this
-        # latitude level that are the closest to the sphere
-        xyz = np.zeros((DRes ** 2, 3))
-        # Make an xy-plane version, since z is constant
-        xy = np.mgrid[
-             -radius: radius + 1e-10: resStep,
-             -radius: radius + 1e-10: resStep,
-             ].reshape(2, -1).T
-        xyz[:, :2] = xy
-        xyz[:, 2] = radius * np.cos(np.pi / 2 - lat)
-        xyz = xyz.reshape((DRes, DRes, 3))
-        # Calculate distance from each point in this plane
-        # to the origin. Anything that is more than the
-        # square of resStep gets filtered out.
-        dists = np.abs(np.linalg.norm(xyz, axis=2) - radius)
-        xyz = xyz[np.where(dists < resStep)]
-        # Now, for each point in xyz, we find the two defined points
-        # that are closest and interpolate between those two points.
-        for point in xyz:
-            # Get the closest points, the vectors
-            # at those points, and the distances
-            locs, vects, dists = find2Points(point, exactLats, vects3DLats)
-            # Calculate a weighted average of the
-            # first vector to the second vector
-            weights = 1 - dists / np.sum(dists)
-            # Two things we need to calculate:
-            # the direction and the magnitude.
-            # The magnitude of our vectors are all
-            # one for testing, but actual data is not.
+    # So here is the plan: We will first make
+    # a 3D (really 4D) array where each element
+    # is the actual [x, y, z] coordinate across
+    # the whole box.
+    # Then, we go through each point, and see if it's
+    # "close enough" to the surface to the sphere
+    # (not inside).
+    # If it is, then we calculate that point's
+    # latitude and longitude values, using a conversion
+    # to spherical coordinates.
+    # We then check which 2 latitudes and longitudes we
+    # have actual data on. Each of our [x,y,z]
+    # points will fall in a rectangle where the corners
+    # have actual wind data.
+    # At this point, we can interpolate horizontally
+    # (using the 2 longitudal sides), and interpolate
+    # vertically (using the 2 latitude sides) across
+    # both angle and magnitude. Do this for every
+    # point and we have finished our interpolation.
 
-            # For direction, we can calculate
-            # the angle between the two vectors,
-            # since we're in an xy-plane at a z.
-            thetaDiff = np.arccos(np.dot(vects[0], vects[1]) /
-                                  (np.linalg.norm(vects[0]) * np.linalg.norm(vects[1])))
-            rotTheta = weights[1] * thetaDiff  # Go this much of the way
-            # rotTheta just tells magnitude, but does not tell
-            # the direction of the turn. This is in the following:
-            if vects[0, 0] * vects[1, 1] - vects[0, 1] * vects[1, 0] < 0:
-                rotTheta *= -1  # Rotate towards the right, otherwise left
-            # Magnitude - weighted average
-            magnitude = np.dot(weights, np.linalg.norm(vects, axis=1))
-            # Take our first vector, rotate it by rotTheta
-            # about the z-axis (because in the same xy-plane)
-            # and rescale it by newMag. Rotation matrix time!
-            newVector = np.dot([
-                [np.cos(rotTheta), -np.sin(rotTheta), 0],
-                [np.sin(rotTheta), np.cos(rotTheta), 0],
-                [0, 0, 1]
-            ], vects[0])
+    ### LET'S GET STARTED! ###
 
-            # OKAY we have our new vector, and where we need to place it.
-            # Now, we need to find the point in our box space that is
-            # closest to our actual point. Thankfully, because
-            # we computed our point space based upon the resolution,
-            # it will match up exactly. It's basically an
-            # arithmetic progression
-            xi, yi, zi = tuple(np.array((point + radius) / resStep, dtype=int))  # Really - (-boxRad)
-            fgaVectors[xi, yi, zi] = newVector * windScale
-            # AND WE'RE DONE WITH INTERPOLATION
-    print('Interpolation complete!')
+    # First we need a way to iterate through all possible
+    # point values, along with their respective indices,
+    # so we can assign the proper values in fgaVectors.
+    # Because this a square box, this is made easy
+    # through itertools.product(). We make one each for
+    # the point values and index values. We then iterate
+    # through them at the same time using zip().
+    valueSpace = np.arange(-radius, radius + 1e-10, resStep)
+    indexSpace = np.arange(len(valueSpace))
+    valueProduct = product(valueSpace, valueSpace, valueSpace)
+    indexProduct = product(indexSpace, indexSpace, indexSpace)
+    for point, indices in zip(valueProduct, indexProduct):
+        x, y, z = point
+        xi, yi, zi = indices
+        distFromOrigin = np.linalg.norm([x,y,z])
+        # If the distance from the point to
+        # the origin is "close enough", then we
+        # interpolate on this point. Here, "close enough"
+        # means above the sphere and less than resStep away.
+        if 0 <= distFromOrigin - radius < resStep:
+            # print('({}, {}, {}) ==> '.format(x, y, z), end='')
+            # print('[{}, {}, {}] ==> '.format(xi, yi, zi), end='')
+
+            # Find the spherical coordinates of this point.
+            # The method returns the latitude and longitude
+            # in the correct ranges.
+            rho, lat, lon = cart2sph(x, y, z)
+            # print('({:.2f}, {:.2f}, {:.2f}) ==> '.format(rho, lat * 180 / np.pi, lon * 180 / np.pi), end='')
+
+            # Use numpy's fancy searchsorted function to find
+            # the closest locations for latitude and longitude.
+            # searchsorted returns a right associated index.
+            # The reason for the mod is that if the value is
+            # off the deep end, then searchsorted returns
+            # the length of the array, which is invalid index.
+            # So mod the length the array to turn it 0, and
+            # the left index (which should wrap around), works as
+            # intended.
+
+            boxLocs, weightLat, weightLon = findBoxPointsAndWeights(lat, lon, latPoints, lonPoints)
+
+            # Point 4 is assumed to be diagonally opposite Point 1,
+            # and Point 2 is assumed to be horizontally opposite Point 1.
+            # First we need to find the two vectors directly
+            # above and below (left and right also works) our
+            # target location.
+            interAbove = (1-weightLon) * vects3D[boxLocs[0]] + weightLon * vects3D[boxLocs[1]]
+            interBelow = (1-weightLon) * vects3D[boxLocs[2]] + weightLon * vects3D[boxLocs[3]]
+            # Now we interpolate vertically
+            # between these two points
+
+            interedVec = (1-weightLat) * interBelow + weightLat * interAbove
+
+            # print('({:.2f}, {:.2f}, {:.2f})'.format(interedVec[0], interedVec[1], interedVec[2]))
+
+            # We're done with the interpolation with this vector,
+            # so now using the index values all the way above, assign
+            # to fgaVectors...scaling as necessary
+            fgaVectors[xi, yi, zi] = interedVec * windScale
 
     ############# WRITING TO FILE ##############
 
